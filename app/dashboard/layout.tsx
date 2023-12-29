19c10d654ea7f9755c1c9c982125c2b507d81b8f
Franck <franckvienot7@gmail.com>
2023-12-29 17:36:35 +0100

    made initial configuration && dashboard page && modeToggle

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/themeProvider"
-import "./globals.css"
+import "../globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from "@/components/modeToggle"
+
+function Dashboard() {
+  return (
+    <>
+      <div>Welcome to the dashboard</div>
+      <ModeToggle />
+    </>
+  )
+}
+
+export default Dashboard
