9f3b3c968686119216eb35cce544d6955230c16d
Franck <franckvienot7@gmail.com>
2024-01-01 22:04:48 +0100

    added logo to navbar

--- components/mainNavbar.tsx
+++ components/mainNavbar.tsx
@@ -1,12 +1,18 @@
 "use client"
 import Link from "next/link"
+import Image from "next/image"
 import { ModeToggle } from "./modeToggle"
+import NDLogo from "@/public/images/logo/ND_Logo.svg"
 
 function mainNavbar() {
   return (
     <>
       <div className="container flex justify-around">
-        <div className="bg-purple-600 flex mx-3 px-4 py-5 justify-center">logo</div>
+        <div className="bg-white flex mx-3 px-4 py-5 justify-center">
+          <Link href={"/"}>
+            <Image src={NDLogo} alt="Nouvelles Donnes" width={150} height={50} />
+          </Link>
+        </div>
         <div className="bg-purple-600 ">
           <ul className="flex mx-3 py-5 justify-around">
             <li className="px-3">
